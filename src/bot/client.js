2928a18185600dcf3c25b0f93ee21ae997941702
redejot <redejot@gmail.com>
2026-02-17 00:25:14 +0100

    Connection to internet functionality

--- src/bot/client.js
+++ src/bot/client.js
@@ -59,6 +59,52 @@ for (const file of eventFiles) {
 }
 
 // Wyeksportowanie komendy startBot i zalogowanie bota tokenem
-export const startBot = () => {
+/*export const startBot = () => {
   client.login(env.token);
+};*/
+let isRetrying = false;
+
+async function connectToDiscord() {
+  try {
+    await client.login(env.token);
+    console.log('Bot połączył się z siecią.');
+    isRetrying = false;
+  } catch (error) {
+    isRetrying = true;
+    console.error('Błąd podczas połączenia do Discorda:', error);
+    console.log('Ponowne połączenie za 5 sekund...');
+    setTimeout(connectToDiscord, 5000);
+  }
+}
+
+export const startBot = () => {
+  connectToDiscord();
 };
+
+client.on('shardError', (err) => {
+  isRetrying = true;
+  console.error("Błąd połączenia :", err);
+})
+
+client.on('shardDisconnect', () => {
+  isRetrying = true;
+  console.log('Bot stracił połączenie z siecią.');
+});
+
+client.on('shardReconnecting', () => {
+  console.log('Próba ponownego nawiązania połączenia...');
+  isRetrying = true;
+});
+
+client.on('shardResume', () => {
+  console.log('Bot ponownie połączony z siecią.');
+  isRetrying = false;
+});
+
+client.on('shardReady', () => {
+  if (isRetrying) {
+    console.log(`Bot zalogowany ponownie i jest połączony z siecią.`);
+    isRetrying = false;
+  }
+})
+
